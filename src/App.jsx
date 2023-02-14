0c6f42acfac8cbcbbfce6c897ab7c697b072e07f
diegodestf <diegodesteffano@gmail.com>
2023-02-14 11:44:38 -0300

    Added React Helmet details

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { Helmet } from 'react-helmet';
 import { Car } from './components/Car';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <html lang="en" />
+        <title>3 Column Preview</title>
+      </Helmet>
       <Car />
     </div>
   );
